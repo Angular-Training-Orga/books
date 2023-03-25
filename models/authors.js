9423eb29f1fe219620de11d2f048cd693b44cbb5
Younes <younesesebbar089@gmail.com>
2023-03-25 23:07:51 +0000

    updated the authors model

--- models/authors.js
+++ models/authors.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const authorSchema = new mongoose.Schema({
+const authorSchema = new Schema({
   firstName: String,
   lastName: String,
   books: [
     {
-      type: mongoose.Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Book",
     },
   ],
